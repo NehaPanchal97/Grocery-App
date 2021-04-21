11acf34a383d388f51e5514d3d43d99f49826d87
Vikas Sharma <vikass@dewsolutions.in>
2021-04-21 14:54:34 +0530

    added viewModel dependency

--- app/src/main/java/com/grocery/app/MainActivity.kt
+++ app/src/main/java/com/grocery/app/MainActivity.kt
@@ -2,6 +2,7 @@ package com.grocery.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LiveData
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
