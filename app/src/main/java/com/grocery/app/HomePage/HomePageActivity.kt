56849f0454026398200dad5b2454385470fee0de
suma99 <kattepogu.suma@tatadigital.com>
2021-04-28 17:35:56 +0530

    third UI Screen for Homepage

--- app/src/main/java/com/grocery/app/HomePage/HomePageActivity.kt
+++ app/src/main/java/com/grocery/app/HomePage/HomePageActivity.kt
@@ -1,11 +1,14 @@
 package com.grocery.app.HomePage
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.grocery.app.R
+import com.grocery.app.auth.SsoLoginActivity
 import kotlinx.android.synthetic.main.bottom_navigation_bar.*
 import kotlinx.android.synthetic.main.category_group.*
 
@@ -25,6 +28,14 @@ class HomePageActivity: AppCompatActivity() {
 
     fun onClickBtnMore(v: View?) {
         val fragment = CategoryTypesFragment()
+        val fragmentTransaction=supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
+    fun onItemClick(v:View?){
+        val fragment = CategoryItemsFragment()
         val fragmentTransaction=supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.fragment_container, fragment)
         fragmentTransaction.addToBackStack(null)
