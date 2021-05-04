c22256ed774e7b71980ca8af56385443d2293996
NehaPanchal97 <neha.panchal@tatadigital.com>
2021-05-04 11:40:53 +0530

    saving login data

--- app/src/main/java/com/grocery/app/HomePage/HomePageActivity.kt
+++ app/src/main/java/com/grocery/app/HomePage/HomePageActivity.kt
@@ -4,23 +4,67 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.grocery.app.R
+import com.grocery.app.auth.AuthenticationFragment
+import com.grocery.app.constant.USER
+import com.grocery.app.extensions.showError
+import com.grocery.app.extensions.showSuccess
+import com.grocery.app.extras.Result
+import com.grocery.app.models.User
+import com.grocery.app.utils.PrefManager
+import com.grocery.app.viewModels.AuthViewModel
 import kotlinx.android.synthetic.main.bottom_navigation_bar.*
 import kotlinx.android.synthetic.main.category_group.*
 
 
 class HomePageActivity: AppCompatActivity() {
 
-
+    private val prefManager by lazy { PrefManager.getInstance(this) }
+    private lateinit var viewModel : AuthViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        viewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
+        observeData()
+//       switchFragment()
+        val user = prefManager.get<User>(USER)
+        user?.let {
+            switchFragment()
+        }?: kotlin.run { viewModel.fetchUserInfo() }
+    }
+
+
+    private fun switchFragment(fragment: Fragment = HomeFragment()) {
         bottomNavigationBar.background = null
-        val fragment = HomeFragment()
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragment_container, fragment)
         transaction.commit()
+    }
+
+    private fun observeData(){
+        viewModel.fetchUserLiveData.observe(this, Observer {
+            when(it.type){
+                Result.Status.LOADING -> {
+
+                }
+                Result.Status.SUCCESS -> {
+                  currentFocus?.showSuccess("updated")
+                    prefManager.put(USER,it.data)
+                    it.data?.let {
+                        switchFragment()
+                    }?: kotlin.run {
+                        switchFragment(AuthenticationFragment())
+                    }
+                }
+                Result.Status.ERROR -> {
+                  currentFocus?.showError("error")
+                }
+            }
+        })
     }
 
     fun onClickBtnMore(v: View?) {
--- app/src/main/java/com/grocery/app/auth/AuthenticationFragment.kt
+++ app/src/main/java/com/grocery/app/auth/AuthenticationFragment.kt
@@ -8,16 +8,20 @@ import android.view.ViewGroup
 import androidx.core.widget.doAfterTextChanged
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.grocery.app.constant.USER
 import com.grocery.app.databinding.FragmentAuthenticationBinding
 import com.grocery.app.extensions.authUser
 import com.grocery.app.extensions.showError
 import com.grocery.app.extensions.showSuccess
 import com.grocery.app.extras.Result
+import com.grocery.app.models.User
+import com.grocery.app.utils.PrefManager
 import com.grocery.app.viewModels.AuthViewModel
 
 
 class AuthenticationFragment : Fragment() {
-
+    private val prefManager by lazy { PrefManager.getInstance(requireContext()) }
+//    private lateinit var sharedPreferenceForLogin: SharedPreferenceForLogin
     private lateinit var binder: FragmentAuthenticationBinding
     private lateinit var viewModel: AuthViewModel
 
@@ -27,6 +31,7 @@ class AuthenticationFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binder = FragmentAuthenticationBinding.inflate(inflater, container, false)
+//        sharedPreferenceForLogin = SharedPreferenceForLogin(requireContext())
         return binder.root
     }
 
@@ -36,13 +41,14 @@ class AuthenticationFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
         setupView()
         observe()
+        setUpViewWithData()
+        observeData()
     }
 
     private fun observe() {
         viewModel.updateUserLiveData.observe(viewLifecycleOwner, Observer {
             when (it.type) {
                 Result.Status.LOADING -> {
-
                 }
                 Result.Status.SUCCESS -> {
                     binder.root.showSuccess("User Info Updated.")
@@ -53,21 +59,48 @@ class AuthenticationFragment : Fragment() {
             }
         })
     }
-
+    private fun saveAccountDetails(user: User){
+//        sharedPreferenceForLogin.saveAccountDetails(_user)
+        prefManager.put(USER,user)
+    }
     private val _user
         get() = viewModel.user
 
     private fun setupView() {
         binder.mobEt.setText(authUser?.phoneNumber)
-        binder.nameEt.doAfterTextChanged { _user.name = it.toString() }
-        binder.addressEt.doAfterTextChanged { _user.address = it.toString() }
+        binder.nameEt.doAfterTextChanged { _user?.name = it.toString() }
+        binder.addressEt.doAfterTextChanged { _user?.address = it.toString() }
         binder.btnLogout.setOnClickListener { viewModel.updateUserInfo() }
 
-        _user.phone = authUser?.phoneNumber
-        _user.id = authUser?.uid
-        _user.name = authUser?.displayName
-        _user.address = binder.addressEt.text.toString()
+        _user?.phone = authUser?.phoneNumber
+        _user?.id = authUser?.uid
+        _user?.name = authUser?.displayName
+        _user?.address = binder.addressEt.text.toString()
 
+    }
+
+    private fun observeData(){
+        viewModel.fetchUserLiveData.observe(viewLifecycleOwner, Observer {
+            when(it.type){
+                Result.Status.LOADING -> {
+
+                }
+                Result.Status.SUCCESS -> {
+                    it.data
+                    binder.root.showSuccess("updated")
+                    it.data?.let { it1 -> saveAccountDetails(it1) }
+                }
+                Result.Status.ERROR -> {
+                    binder.root.showError("error")
+                }
+            }
+        })
+    }
+
+    private fun setUpViewWithData(){
+        binder.nameEt.setText(authUser?.displayName)
+        binder.addressEt.setText(binder.addressEt.text.toString())
+        viewModel.fetchUserInfo()
     }
 
 
