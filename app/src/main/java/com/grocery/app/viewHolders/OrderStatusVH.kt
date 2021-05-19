1832f93302b43ab4d2ed6102554ebee9ebcf2543
suma99 <kattepogu.suma@tatadigital.com>
2021-05-19 19:09:59 +0530

    changes in Search Container

--- app/src/main/java/com/grocery/app/viewHolders/OrderStatusVH.kt
+++ app/src/main/java/com/grocery/app/viewHolders/OrderStatusVH.kt
@@ -9,6 +9,7 @@ import com.grocery.app.models.OrderStatus
 class OrderStatusVH (private val binder: OrderStatusItemBinding):
         BaseVH<OrderStatusItemBinding, OrderStatus>(binder){
 
+
      override fun bind(data:OrderStatus) {
 
          val statusBg = if (data.completed==true)
@@ -16,11 +17,15 @@ class OrderStatusVH (private val binder: OrderStatusItemBinding):
          else R.drawable.cart_uncheck_circle
 
 
-         binder.statusText.text = data.status
+          if (data.status == com.grocery.app.constant.OrderStatus.DELIVERED.title)
+             binder.connectingLine.visible(false)
+         else binder.connectingLine.visible(true)
 
+         binder.statusText.text = data.status
          binder.description.text = data.description
          binder.userResponseTime = data.createdAt?.toDate().toString()
          binder.check.background = ContextCompat.getDrawable(itemView.context, statusBg)
+
          binder.executePendingBindings()
      }
 
