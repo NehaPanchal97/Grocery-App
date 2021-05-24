802174bdc8c570e574be1c3fc690ff22c12e24b6
NehaPanchal97 <neha.panchal@tatadigital.com>
2021-05-24 11:53:55 +0530

    DiscountPage Activity created

---
+++ app/src/main/java/com/grocery/app/activities/DiscountPageActivity.kt
@@ -0,0 +1,12 @@
+package com.grocery.app.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.grocery.app.R
+
+class DiscountPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_discount_page)
+    }
+}
