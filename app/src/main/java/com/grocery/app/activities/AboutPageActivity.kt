40ded2ab12d05ce58b9b9b8e878e17201438338a
NehaPanchal97 <neha.panchal@tatadigital.com>
2021-05-31 14:24:33 +0530

    adding location and map

--- app/src/main/java/com/grocery/app/activities/AboutPageActivity.kt
+++ app/src/main/java/com/grocery/app/activities/AboutPageActivity.kt
@@ -5,6 +5,10 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.grocery.app.R
 import com.grocery.app.databinding.ActivityAboutPageBinding
 
@@ -15,7 +19,7 @@ class AboutPageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binder = DataBindingUtil.setContentView(this, R.layout.activity_about_page)
         onCallPressed()
-
+        loadLocation()
     }
 
     private fun onCallPressed() {
@@ -24,6 +28,28 @@ class AboutPageActivity : AppCompatActivity() {
             callIntent.data = Uri.parse("tel:+919987384423")
             startActivity(callIntent)
         }
+    }
+
+    private fun loadLocation() {
+        binder.ivLocation.setOnClickListener {
+            val gmmIntentUri = Uri.parse("geo:37.7749,-122.4194")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            mapIntent.resolveActivity(packageManager)?.let {
+                startActivity(mapIntent)
+            }
+        }
+    }
+
+    fun onMapReady(googleMap: GoogleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        val sydney = LatLng(37.7749, -122.4194)
+        googleMap.addMarker(
+            MarkerOptions()
+                .title("Grocery shop")
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
 
